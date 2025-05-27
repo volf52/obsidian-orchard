d5a9f8babe80b9e7c5ad198705763b18b01dc6d8
Arslan <rslnkrmt2552@gmail.com>
2025-05-27 19:54:12 +0500

    use tsgo as lsp

--- .nvim.lua
+++ .nvim.lua
@@ -1,1 +1,12 @@
 vim.cmd([[set runtimepath+=.nvim]])
+
+local tsgopath = vim.fn.executable("tsgo")
+
+local tsgo_enabled = false
+
+if tsgopath and tsgo_enabled then
+	vim.lsp.enable("vtsls", false)
+
+	vim.lsp.enable("tsgo")
+	vim.notify("Using tsgo instead of vtsls", vim.log.levels.WARN)
+end
