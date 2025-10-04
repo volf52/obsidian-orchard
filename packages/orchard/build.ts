78749c0a023d70fa18dd2b3104571fbce700487f
Arslan <rslnkrmt2552@gmail.com>
2025-10-04 12:14:20 +0200

    updated lint config

--- packages/orchard/build.ts
+++ packages/orchard/build.ts
@@ -1,6 +1,6 @@
-import builtins from "builtin-modules";
-import { sveltePlugin } from "./svelte-plugin";
-import type { BunPlugin } from "bun";
+import builtins from "builtin-modules"
+import type { BunPlugin } from "bun"
+import { sveltePlugin } from "./svelte-plugin"
 
 // Alias @orchard/core to source (avoid needing prebuilt dist or version bumps)
 const coreAliasPlugin: BunPlugin = {
@@ -8,9 +8,9 @@ const coreAliasPlugin: BunPlugin = {
   setup(build) {
     build.onResolve({ filter: /^@orchard\/core$/ }, () => ({
       path: new URL("../orchard-core/src/index.ts", import.meta.url).pathname,
-    }));
+    }))
   },
-};
+}
 
 Bun.build({
   entrypoints: ["./src/plugin.ts"],
@@ -38,4 +38,4 @@ Bun.build({
     ...builtins,
   ],
   plugins: [coreAliasPlugin, sveltePlugin],
-});
+})
